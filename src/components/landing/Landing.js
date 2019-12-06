f4b7bd728404bbc4684d032af61f0fdd1d43ca93
Daniel Kambich <dkambich@purdue.edu>
2019-12-05 21:29:41 -0500

    Add navbar component

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
+import NavBar from "../navigation/NavBar";
 
 function Landing() {
-  return <h1>Welcome Page</h1>;
+  return (
+    <Fragment>
+      <NavBar />
+      <h1>Welcome Page</h1>
+    </Fragment>
+  );
 }
 export default Landing;
