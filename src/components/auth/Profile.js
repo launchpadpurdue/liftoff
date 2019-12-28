f7bfb5803f80991cc0778c1a276d07d4fe050aab
Daniel Kambich <dkambich@purdue.edu>
2019-12-28 17:41:09 -0600

    Update profile layout

--- src/components/auth/Profile.js
+++ src/components/auth/Profile.js
@@ -7,25 +7,24 @@ import {
   Paper,
   Container,
   Grid,
-  ButtonBase,
   Typography,
   Chip,
-  Divider
+  Divider,
+  Button
 } from "@material-ui/core";
 import { Redirect } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faBrain,
   faDesktop,
-  faGamepad
+  faGamepad,
+  faBug,
+  faRobot,
+  faGlobe
 } from "@fortawesome/free-solid-svg-icons";
 import { faApple, faAndroid } from "@fortawesome/free-brands-svg-icons";
+import { Edit, DeleteForever, Lock } from "@material-ui/icons";
 
 const styles = theme => ({
-  heroContent: {
-    height: "100vh",
-    backgroundColor: theme.palette.background.paper
-  },
   paper: {
     marginTop: theme.spacing(3),
     marginBottom: theme.spacing(3),
@@ -37,20 +36,32 @@ const styles = theme => ({
     }
   },
   image: {
-    width: "100%",
+    width: "75%",
     height: "auto",
+    [theme.breakpoints.up("sm")]: {
+      width: "100%",
+      height: "auto"
+    },
     borderColor: theme.palette.primary.main,
     borderRadius: "50%",
     objectFit: "contain"
   },
-  profileInfo: {
-    [theme.breakpoints.up("sm")]: {
+  edit: { position: "absolute", top: 0 },
+  profile: {
+    [theme.breakpoints.up("xs")]: {
       marginLeft: theme.spacing(2)
     }
   },
+  divider: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2)
+  },
+  name: {
+    [theme.breakpoints.down("xs")]: {
+      textAlign: "center"
+    }
+  },
   skill: {
-    marginRight: theme.spacing(1),
-    marginBottom: theme.spacing(1),
     padding: theme.spacing(1)
   }
 });
@@ -61,64 +72,122 @@ function mapSkillToIcon(skill) {
       return faAndroid;
     case "IOS":
       return faApple;
-    case "ML":
-      return faBrain;
+    case "Machine Learning":
+      return faRobot;
     case "Gaming":
       return faGamepad;
-    case "Web":
+    case "Desktop":
       return faDesktop;
+    case "Web":
+      return faGlobe;
+    default:
+      return faBug;
   }
 }
 
-const skills = ["Android", "IOS", "ML", "Gaming", "Web"];
+const skills = [
+  "Android",
+  "IOS",
+  "Web",
+  "Desktop",
+  "Machine Learning",
+  "Gaming"
+];
 
 class Profile extends Component {
+  renderChip = skill => {
+    const { classes } = this.props;
+    return (
+      <Chip
+        key={skill}
+        className={classes.skill}
+        icon={<FontAwesomeIcon icon={mapSkillToIcon(skill)} />}
+        label={skill}
+        color="secondary"
+      />
+    );
+  };
+
   render() {
     const { classes, auth, profile } = this.props;
     if (!auth.uid) return <Redirect to="/signin"></Redirect>;
-    console.log(classes.skill);
     return (
       <Fragment>
         <NavBar />
         <main className={classes.layout}>
           <Container maxWidth="lg">
             <Paper className={classes.paper}>
-              <Grid container direction="row">
-                <Grid item sm={3} xs={12}>
-                  <ButtonBase className={classes.image}>
-                    <img
-                      border={5}
-                      className={classes.image}
-                      alt="avatar"
-                      src={profile.profilePicture}
-                    />
-                  </ButtonBase>
+              <Grid container>
+                <Grid
+                  item
+                  sm={3}
+                  xs={12}
+                  container
+                  justify="center"
+                  alignItems="center"
+                >
+                  <img
+                    border={5}
+                    className={classes.image}
+                    alt="avatar"
+                    src={profile.profilePicture}
+                  />
                 </Grid>
-                <Grid item className={classes.profileInfo} xs>
-                  <Typography variant="h3" color="textPrimary" gutterBottom>
+                <Grid item className={classes.profile} xs>
+                  <Typography variant="h3" className={classes.name}>
                     {profile.firstName + " " + profile.lastName}
                   </Typography>
-                  <Divider></Divider>
-                  <Grid item container direction="column">
-                    <Typography variant="h6">Skills</Typography>
-                    <Grid item container direction="row">
-                      {skills.map(skill => (
-                        <Chip
-                          className={classes.skill}
-                          icon={
-                            <FontAwesomeIcon icon={mapSkillToIcon(skill)} />
-                          }
-                          label={skill}
-                          clickable
-                          color="secondary"
-                        />
-                      ))}
+                  <Divider className={classes.divider} />
+                  <Grid item container direction="column" spacing={1}>
+                    <Grid item>
+                      <Typography variant="h6">Skills</Typography>{" "}
+                      <Grid item container spacing={1}>
+                        {skills.map(skill => (
+                          <Grid item>{this.renderChip(skill)}</Grid>
+                        ))}
+                      </Grid>
                     </Grid>
-                    <Typography variant="h6">Description</Typography>
-                    <Typography variant="body1">
-                      {profile.description}
-                    </Typography>
+
+                    <Grid item>
+                      <Typography variant="h6">Description</Typography>{" "}
+                      <Typography variant="body1">
+                        {profile.description}
+                      </Typography>
+                    </Grid>
                   </Grid>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Container>
+          <Container maxWidth="lg">
+            <Paper className={classes.paper}>
+              <Grid container spacing={1}>
+                <Grid item>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    startIcon={<Edit />}
+                  >
+                    Edit Profile
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    color="secondary"
+                    variant="contained"
+                    startIcon={<Lock />}
+                  >
+                    Reset Password
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    color="secondary"
+                    variant="contained"
+                    startIcon={<DeleteForever />}
+                  >
+                    Delete Account
+                  </Button>
                 </Grid>
               </Grid>
             </Paper>
