219c518ee0885a42854e7c32e12abe68a5836182
Daniel Kambich <dkambich@purdue.edu>
2020-03-22 20:58:09 -0500

    Complete OrganizerGallery conversion

--- src/components/directories/OrganizerGallery.tsx
+++ src/components/directories/OrganizerGallery.tsx
@@ -1,17 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect, isEmpty, isLoaded } from 'react-redux-firebase';
-// Redux Imports
 import { compose } from 'redux';
 
-// Material UI Imports
 import {
     Container, createStyles, Grid, Theme, Typography, WithStyles, withStyles
 } from '@material-ui/core';
 
 import { filterMembers, FixMeLater, Member, MemberQuery } from '../../constants';
 import NavBar from '../navigation/NavBar';
-// Local Imports
 import { MemberCard, SkeletonCard } from '../utils/Cards';
 import { EmptyData, EmptyQuery, Footer, Header, QueryBar } from '../utils/Utlities';
 
@@ -52,61 +49,59 @@ class OrganizerGallery extends Component<
     return (
       <React.Fragment>
         <NavBar />
-        <main>
-          <Header>
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="textPrimary"
-              gutterBottom
-            >
-              Meet the Organizers
-            </Typography>
-            <Typography
-              variant="h5"
-              align="center"
-              color="textSecondary"
-              paragraph
-            >
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirely.
-            </Typography>
-          </Header>
-          {dataLoaded && !dataEmpty && <QueryBar onQuery={this.onQuery} />}
-          <Container className={classes.cardGrid} maxWidth="md">
-            {!dataLoaded && (
+        <Header>
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            Meet the Organizers
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            paragraph
+          >
+            Something short and leading about the collection below—its contents,
+            the creator, etc. Make it short and sweet, but not too short so
+            folks don&apos;t simply skip over it entirely.
+          </Typography>
+        </Header>
+        {dataLoaded && !dataEmpty && <QueryBar onQuery={this.onQuery} />}
+        <Container className={classes.cardGrid} maxWidth="md">
+          {!dataLoaded && (
+            <Grid container spacing={4}>
+              {[1, 2, 3].map(num => (
+                <SkeletonCard key={num} />
+              ))}
+            </Grid>
+          )}
+          {dataLoaded && dataEmpty && (
+            <EmptyData
+              title="Come back soon!"
+              message="No organizers have signed up yet but come back soon to meet
+            them all!"
+            />
+          )}
+          {dataLoaded && !dataEmpty && filteredMembers.length === 0 && (
+            <EmptyQuery
+              title="No organizers found"
+              message="No organizers were found that matched your query"
+            />
+          )}
+          {dataLoaded && !dataEmpty && filteredMembers.length > 0 && (
+            <Fragment>
               <Grid container spacing={4}>
-                {[1, 2, 3].map(num => (
-                  <SkeletonCard key={num} />
+                {filteredMembers.map(user => (
+                  <MemberCard key={user.id} member={user} />
                 ))}
               </Grid>
-            )}
-            {dataLoaded && dataEmpty && (
-              <EmptyData
-                title="Come back soon!"
-                message="No organizers have signed up yet but come back soon to meet
-            them all!"
-              />
-            )}
-            {dataLoaded && !dataEmpty && filteredMembers.length === 0 && (
-              <EmptyQuery
-                title="No organizers found"
-                message="No organizers were found that matched your query"
-              />
-            )}
-            {dataLoaded && !dataEmpty && filteredMembers.length > 0 && (
-              <Fragment>
-                <Grid container spacing={4}>
-                  {filteredMembers.map(user => (
-                    <MemberCard key={user.id} member={user} />
-                  ))}
-                </Grid>
-              </Fragment>
-            )}
-          </Container>
-        </main>
+            </Fragment>
+          )}
+        </Container>
         <Footer />
       </React.Fragment>
     );
