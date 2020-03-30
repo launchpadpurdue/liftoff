a5d863b20cf6ac4f6901c5c3d3610327280dec89
Daniel Kambich <dkambich@purdue.edu>
2020-03-30 12:41:11 -0500

    Refactor and comment EventDialog.tsx

--- src/components/utils/Dialogs/EventDialog.tsx
+++ src/components/utils/Dialogs/EventDialog.tsx
@@ -9,18 +9,18 @@ import { KeyboardDateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pi
 import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 
 import firebase from '../../../config/firebaseConfig';
-import { Event, eventTypes, FixMeLater } from '../../../constants';
+import { Event, eventTypes } from '../../../constants';
 
 type EventDialogProps = {
   open: boolean;
   event: Event | null;
   eventID: string | null;
-  onClose: (e: Event | null, id: string | null) => void;
+  onClose: (event: Event | null, eventID: string | null) => void;
 };
 
 type EventDialogState = {
   event: Event;
-  hasPassedData: boolean;
+  hasLoaded: boolean;
 };
 
 const emptyEvent = (): Event => ({
@@ -36,48 +36,93 @@ export default class EventDialog extends Component<
   EventDialogProps,
   EventDialogState
 > {
-  state = { event: emptyEvent(), eventID: null, hasPassedData: false };
+  state = { event: emptyEvent(), hasLoaded: false };
 
   static getDerivedStateFromProps(
     props: EventDialogProps,
     state: EventDialogState
   ) {
-    if (props.open && !state.hasPassedData) {
+    // If the dialog is opened but has not loaded
+    if (props.open && !state.hasLoaded) {
+      // Set the state Event to the props Event if it exists, otherwise an new Event
       state.event = props.event ? { ...props.event } : emptyEvent();
-      state.hasPassedData = true;
+      // Set hasLoaded to true, so the state Event is not set again
+      state.hasLoaded = true;
     }
     return state;
   }
 
-  onInput = (changeEvent: ChangeEvent | FixMeLater) => {
-    const property: FixMeLater = changeEvent.target.id
-        ? changeEvent.target.id
-        : changeEvent.target.name,
-      value: FixMeLater = changeEvent.target.value;
-    const event: FixMeLater = this.state.event;
-    if ((property === "duration" && value === "") || !event) return;
+  // Input Handlers
 
-    event[property] = value;
-    this.setState({ event: event });
+  onInput = (
+    property: string,
+    value: string | number | firebase.firestore.Timestamp
+  ) => {
+    // Set the given property and value on the Event in the state
+    this.setState(prevState => ({
+      event: {
+        ...prevState.event,
+        [property]: value
+      }
+    }));
   };
 
+  onDateInput = (time: MaterialUiPickersDate) => {
+    // Convert the MaterialUiPickersDate to a Timestamp
+    const timestamp = firebase.firestore.Timestamp.fromDate(time as Date);
+    // Update the Event in the state
+    this.onInput("time", timestamp);
+  };
+
+  onNumberInput = (changeEvent: ChangeEvent<HTMLInputElement>) => {
+    // Retrieve the property, value, and validatity of the NumberInput
+    const property = changeEvent.target.id,
+      value = changeEvent.target.value,
+      valid = changeEvent.target.validity.valid;
+    // Update the Event in the state, if valid
+    if (valid) this.onInput(property, value !== "" ? parseFloat(value) : 1);
+  };
+
+  onSelectInput = (
+    changeEvent: ChangeEvent<{ name?: string | undefined; value: unknown }>
+  ) => {
+    // Retrieve the property and value of the Select
+    const property = changeEvent.target.name as string,
+      value = changeEvent.target.value as string;
+    // Update the Event in the state
+    this.onInput(property, value);
+  };
+
+  onTextFieldInput = (changeEvent: ChangeEvent<HTMLInputElement>) => {
+    // Retrieve the property and value of the TextField
+    const property = changeEvent.target.id,
+      value = changeEvent.target.value;
+    // Update the Event in the state
+    this.onInput(property, value);
+  };
+
+  // Dialog Handler
+
   closeDialog = (event: Event | null) => {
-    console.log(event, this.props.eventID);
+    // Call the props onClose method with the event and its potential id
     this.props.onClose(event, this.props.eventID);
+    // Set hasLoaded to false to reset state
     this.setState({
-      hasPassedData: false
+      hasLoaded: false
     });
   };
 
   render() {
+    // Retrieve props
     const { open, eventID } = this.props;
-    const { event } = this.state;
-    if (!open) return null;
-    const { title, description, time, location, type, duration } = event;
-    console.log(this.props);
+    // Retrieve the state Event fields
+    const {
+      event: { title, description, time, location, type, duration }
+    } = this.state;
+    // Convert the Event Timestamp to a Date, to work with KeyboardDateTimePicker
     const timeAsDate = time.toDate();
     return (
-      <Dialog fullWidth open={open} onClose={() => this.closeDialog(null)}>
+      <Dialog fullWidth onClose={() => this.closeDialog(null)} open={open}>
         <DialogTitle>{eventID ? "Edit" : "Create"} Event Details</DialogTitle>
         <DialogContent>
           <DialogContentText>
@@ -87,48 +132,41 @@ export default class EventDialog extends Component<
             autoFocus
             fullWidth
             id="title"
-            margin="dense"
-            defaultValue={title}
             label="Event Name"
+            margin="dense"
+            onChange={this.onTextFieldInput}
             placeholder="ex. Pairing Night"
-            onChange={this.onInput}
+            value={title}
           />
           <TextField
             autoFocus
             fullWidth
             id="location"
-            margin="dense"
-            defaultValue={location}
             label="Event Location"
+            margin="dense"
+            onChange={this.onTextFieldInput}
             placeholder="ex. LWSN B155"
-            onChange={this.onInput}
+            value={location}
           />
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
             <KeyboardDateTimePicker
+              autoOk
+              format="MM/dd/yyyy HH:mm"
               fullWidth
-              placeholder="MM/DD/YYYY hh:mm"
-              variant="inline"
               id="time"
               label="Event Date and Time"
-              value={timeAsDate}
-              autoOk
               margin="dense"
-              onChange={(time: MaterialUiPickersDate) =>
-                this.setState(prevState => ({
-                  event: {
-                    ...prevState.event,
-                    time: firebase.firestore.Timestamp.fromDate(time as Date)
-                  }
-                }))
-              }
-              format="MM/dd/yyyy HH:mm"
+              onChange={this.onDateInput}
+              placeholder="MM/DD/YYYY hh:mm"
+              value={timeAsDate}
+              variant="inline"
             />
           </MuiPickersUtilsProvider>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
+            <Grid item sm={6} xs={12}>
               <FormControl fullWidth margin="dense">
                 <InputLabel>Event Type</InputLabel>
-                <Select value={type} onChange={this.onInput} name="type">
+                <Select name="type" onChange={this.onSelectInput} value={type}>
                   {eventTypes.map(type => (
                     <MenuItem key={type} value={type}>
                       {type.charAt(0).toUpperCase() + type.slice(1)}
@@ -137,39 +175,39 @@ export default class EventDialog extends Component<
                 </Select>
               </FormControl>
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item sm={6} xs={12}>
               <TextField
-                inputProps={{ min: 1 }}
-                margin="dense"
                 fullWidth
-                label="Duration (minutes)"
                 id="duration"
+                inputProps={{ min: 1 }}
+                label="Duration (minutes)"
+                margin="dense"
+                onChange={this.onNumberInput}
                 type="number"
-                onInput={this.onInput}
                 value={duration}
               />
             </Grid>
           </Grid>
           <TextField
+            fullWidth
             id="description"
             label="Event Description"
             margin="dense"
-            placeholder="Add a description of the event"
-            onChange={this.onInput}
-            defaultValue={description}
-            fullWidth
-            rowsMax="4"
             multiline
+            onChange={this.onTextFieldInput}
+            placeholder="Add a description of the event"
+            rowsMax="4"
+            value={description}
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => this.closeDialog(null)} color="primary">
+          <Button color="primary" onClick={() => this.closeDialog(null)}>
             Cancel
           </Button>
           <Button
-            onClick={() => this.closeDialog(this.state.event)}
-            color="primary"
             autoFocus
+            color="primary"
+            onClick={() => this.closeDialog(this.state.event)}
           >
             Confirm
           </Button>
